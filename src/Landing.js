63b92550ca73495677c9b9dfd5c812297b76c046
codeKelly <KellyDeLavergne1@gmail.com>
2019-11-16 04:47:22 -0500

    initial

---
+++ src/Landing.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import {View,Text,ImageBackground} from 'react-native';
+import {Button} from 'native-base';
+
+var myBackground = require('../assets/icons/landing.jpg');
+
+class Landing extends React.Component {
+  render() {
+    return(
+      <View>
+        <ImageBackground source={myBackground} style={{width: '100%', height: '100%'}}>          
+          <View style={styles.viewStyle}>
+            <Text style={styles.titleStyle}>Welcome to PokemonFun!</Text>
+            <Button
+              block={true}
+              style={styles.buttonStyle}
+              onPress={()=>this.props.switchScreen('search')}
+            >
+              <Text style={styles.buttonText}>Start Searching</Text>
+            </Button>
+          </View>
+        </ImageBackground>
+      </View>
+    )
+  }
+}
+
+const styles = {
+  viewStyle: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  titleStyle: {
+    fontSize: 30,
+    color: '#000',
+    alignItems: 'center',
+    fontWeight: 'bold' 
+  },
+  buttonStyle: {
+    margin: 10
+  },
+  buttonText: {
+    color: 'white'
+  }
+}
+
+export default Landing;
